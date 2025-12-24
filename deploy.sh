334de47a7d6b43029daf5d5af6d046c72730d74c
GundlapalliLokeswarRaju <lokeshgundlapalli143@gmail.com>
2025-12-24 23:10:46 +0530

    add download curriculamns in prod

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 set -e  # Exit on any error
 
 # Go to staging project folder
-cd /home/ubuntu/stage/lyntra_website
+cd /home/ubuntu/prod/lyntra_website
 
-# Pull latest code from stage branch
+# Pull latest code from prod branch
 git config pull.rebase false
-git pull origin stage
-cd /home/ubuntu/stage/lyntra_website/bigclasses.ai
+git pull origin prod
+cd /home/ubuntu/prod/lyntra_website/bigclasses.ai
 rm -rf node_modules package-lock.json
 cd ..
 
 # Bring down only the STAGING containers
-docker-compose -f docker-compose.stage.yml -p stage down
+docker-compose -f docker-compose.prod.yml -p prod down
 
 # Build and start the staging stack
-docker-compose -f docker-compose.stage.yml -p stage up -d --build
+docker-compose -f docker-compose.prod.yml -p prod up -d --build
 
 # Test and restart Nginx
 sudo nginx -t
