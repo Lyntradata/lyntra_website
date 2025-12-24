4198303e0be469b933720b674655fc17599a3149
GundlapalliLokeswarRaju <lokeshgundlapalli143@gmail.com>
2025-12-24 22:36:10 +0530

    add download curriculamns

--- bigclasses.ai/src/lib/axiosConfig.ts
+++ bigclasses.ai/src/lib/axiosConfig.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { toast } from '@/hooks/use-toast';
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://www.stage.lyntradata.com/api',
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://www.stage.lyntradata.com/api',
+  // baseURL: 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },
